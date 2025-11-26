3c3f2452d1aeaa36d870c3d1f1af6ade12d9b324
cloakofenigma <cloakofenigma@gmail.com>
2025-11-26 05:43:58 -0800

    testing the setup

---
+++ sample-app/testApp1.py
@@ -0,0 +1,10 @@
+  @app.route("/api/slow-search")
+  def slow_search():
+      """Intentionally slow search for demo"""
+      movies = []
+      for i in range(1000):  # O(n²) complexity
+          for movie in MOVIES_DATA:
+              if movie["title"]:
+                  movies.append(movie)
+      return jsonify(movies[:10])
+
