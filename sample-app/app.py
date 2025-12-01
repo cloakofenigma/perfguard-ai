9a72c53832721f47fe8f2d5de703a0f4e1fdbf19
cloakofenigma <cloakofenigma@gmail.com>
2025-12-01 01:36:56 -0800

    testing pr1

--- sample-app/app.py
+++ sample-app/app.py
@@ -7,6 +7,18 @@ import json
 from movies_data import get_all_movies, get_movie_by_id, search_movies, get_top_rated_movies
 from slow_app_new import register_slow_routes
 
+
+from slow_performance import slow_route_handler
+
+
+
+@app.route('/slow-test')
+  def slow_test():
+      """Test route with intentional performance degradation"""
+      result = slow_route_handler()
+      return jsonify(result)
+
+
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
