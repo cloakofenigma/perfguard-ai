d65e5d6a90d48f1d7d7c3f063bf0b09ae214713b
cloakofenigma <cloakofenigma@gmail.com>
2025-12-02 23:11:11 -0800

    testing slow code

--- sample-app/mega_slow.py
+++ sample-app/mega_slow.py
@@ -1,9 +1,6 @@
-  """Mega slow function for testing"""
-  import time
-
-  def mega_slow():
-      """Sleeps for 2 seconds - much slower than baseline"""
-      time.sleep(2.0)
-      return "very slow"
-  EOF
-
+"""Mega slow function for testing"""
+import time
+def mega_slow():
+  """Sleeps for 2 seconds - much slower than baseline"""
+  time.sleep(2.0)
+  return "very slow"
